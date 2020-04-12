033f215835f3fc711ad67bbd9fd70cd9a08a05ea
Isale <isale.bryan@gmail.com>
2020-04-12 19:16:00 +0300

    Friend

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,7 +32,7 @@ function toggleMenu() {
 }
 // Typed JS
 var typed = new Typed('.type', {
-    strings: ["Web Developer ", "Graphic Artist ", "Entrepreneur ", "Lifelong Learner.^1000 "],
+    strings: ["Web Developer ", "Graphic Artist ", "Entrepreneur ","Lifelong Learner", "Friend.^1000 "],
     typeSpeed: 80,
     loop: true,
     backDelay: 1100,
