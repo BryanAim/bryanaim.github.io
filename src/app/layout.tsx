aecb763aafc4ac14bccaf3f5e178b32ef290343e
Brian Isale <isale.bryan@gmail.com>
2026-03-21 22:49:10 +0300

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning>
         <Header />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
