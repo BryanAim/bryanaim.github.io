e19089bf7ab60c805e4122970e64f88576bf6b80
Brian Isale <isale.bryan@gmail.com>
2026-03-21 22:51:56 +0300

    start counting visitors and page views

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -37,6 +38,7 @@ export default function RootLayout({
         <Header />
         {children}
         <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
