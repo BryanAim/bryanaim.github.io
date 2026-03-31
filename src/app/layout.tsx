17e3d772139c64e40cbe7b8d4c26a74b8c91df34
Brian Isale <isale.bryan@gmail.com>
2026-03-31 11:59:14 +0300

    fix favicon issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -226,7 +226,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(faqJsonLd) }}
         />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="/fonts/css/all.min.css" />
       </head>
       <body className={inter.className} suppressHydrationWarning>
