23c05a6866c720ac00c9f344598445239459e35f
Brian Isale <isale.bryan@gmail.com>
2026-04-30 23:50:21 +0300

    remove sanity io

--- next.config.js
+++ next.config.js
@@ -43,7 +43,6 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'cdn.sanity.io' },
       { protocol: 'https', hostname: 'res.cloudinary.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
